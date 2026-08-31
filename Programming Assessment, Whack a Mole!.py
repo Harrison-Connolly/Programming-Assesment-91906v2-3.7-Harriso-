8dc5c5651dada1428bbe11edbac5781f9a46e017
Harrison-Connolly <Harrisonconnolly@rhs.school.nz>
2026-08-31 15:01:06 +1200

    ADDED CODE FOR WINDOW

--- Programming Assessment, Whack a Mole!.py
+++ Programming Assessment, Whack a Mole!.py
@@ -1,1 +1,18 @@
-print("Hello, World!")
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+class MyWindow(QMainWindow):
+   def __init__(self):
+       super().__init__()
+       self.initUI()
+
+   def initUI(self):
+       self.setWindowTitle("This is a window")
+       self.setGeometry(100, 100, 400, 200)
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   this_window = MyWindow()
+   this_window.show()
+   sys.exit(app.exec())
+
