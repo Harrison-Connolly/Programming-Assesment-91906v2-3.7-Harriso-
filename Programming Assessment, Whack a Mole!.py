6302353d91725d027f2122b521a6e36697665c53
Harrison-Connolly <Harrisonconnolly@rhs.school.nz>
2026-09-20 19:34:04 +1200

    Started working on fixing the dynamic grid, and functionality of mole placement.

--- Programming Assessment, Whack a Mole!.py
+++ Programming Assessment, Whack a Mole!.py
@@ -1,3 +1,4 @@
+from ast import While
 import sys
 import random
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout, QStackedWidget, QWidget, QVBoxLayout, QLabel, QMessageBox
@@ -8,6 +9,10 @@ class MyWindow(QMainWindow):
 
         self.stack = QStackedWidget()
         self.setCentralWidget(self.stack)
+        self.value = 3  # default grid size
+        self.buttons = []
+        self.mole_row = None
+        self.mole_col = None
 
         self.initUI()
 
@@ -21,25 +26,37 @@ class MyWindow(QMainWindow):
         self.switch_button = QPushButton("3X3 layout",)
         self.switch_button2 = QPushButton("5X5 layout")
         self.switch_button.move(100, 100)
-        self.switch_button.clicked.connect(self.show_grid_page)
-        self.switch_button2.clicked.connect(self.show_grid_page)
+        self.switch_button.clicked.connect(lambda: self.start_game(3))
+        self.switch_button2.clicked.connect(lambda: self.start_game(5))
         print("3X3 layout button created") 
         page1_layout.addWidget(self.switch_button)
         page1_layout.addWidget(self.switch_button2)
-        self.page1.setLayout(page1_layout)
+        self.page1.setLayout(page1_layout)        
 
-        if self.switch_button.clicked:
-            print("3X3 layout button clicked")
-            int.value = 3
-        elif self.switch_button2.clicked:
-            print("5X5 layout button clicked")
-            int.value = 5
-            print(self.value)
-
-        
-
-        #second page with the 3x3 grid layout.q
         self.page2 = QWidget()
+        self.stack.addWidget(self.page1)
+        self.stack.addWidget(self.page2)
+
+    def start_game(self, size):
+        self.value = size
+        self.show_grid_page
+        print(f"{size}X{size} layout button clicked")
+        self.build_grid()
+        self.stack.setCurrentWidget(self.page2)
+        self.placemole()
+
+    def build_grid(self):
+    # removes buttons from the previous game.
+        old_layout = self.page2.layout()
+        if old_layout is not None:
+            while old_layout.count():
+                item = old_layout.takeAt(0)
+                widget = item.widget()
+                if widget is not None:
+                    widget.deleteLater()
+            QWidget().setLayout(old_layout)
+
+ #second page with the 3x3 grid layout.q
         grid_layout = QGridLayout()
         self.buttons = []
         for row in range ({self.value}):
@@ -49,29 +66,16 @@ class MyWindow(QMainWindow):
                 button.clicked.connect(lambda clicked, r=row, c=col: self.button_click(r, c))
                 self.buttons.append(button)
         self.page2.setLayout(grid_layout)
-
-        self.stack.addWidget(self.page1)
-        self.stack.addWidget(self.page2)
-        self.mole_row = None
-        self.mole_col = None
-    #another page with the 5x5 grid layout
-
-    def show_grid_page(self):
-        self.stack.setCurrentWidget(self.page2)
-        self.placemole()
     def get_button(self, row, col):
-        return self.buttons[row * {self.value} + col]
+        return self.buttons[row * self.value + col]
+
     def placemole(self):
         for button in self.buttons:
             button.setText("Empty")
-
-        while True:
-            new_row = random.randint(0, self.value - 1)
-            new_col = random.randint(0, self.value - 1)
-            if (new_row, new_col) != (self.mole_row, self.mole_col):
-                break
-        self.mole_row, self.mole_col = new_row, new_col
-        self.get_button(new_row, new_col).setText("Mole")
+    While True:
+        self.mole_row = random.randint(0, self.value - 1)
+        self.mole_col = random.randint(0, self.value - 1)
+            
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
